ba03f34c9c63c23c5f42a3181e3c6dfe3a30c98f
Yad <lolyer47@gmail.com>
2024-02-14 01:35:41 +0300

    Added multifloor system, removed onMap display of completed path

--- MainEXP/js/map.js
+++ MainEXP/js/map.js
@@ -7,6 +7,7 @@
 			building_id: '', 
 			target_id: ''
 		};
+		parse_pathname();
 		
 		colors =
 		{
@@ -20,7 +21,7 @@
 			white: "#FFFFFF",
 		};
 			
-	//Const
+	//Variables
 		const container = document.getElementById("body_id");
 		const w = window.innerWidth;
 		const h = window.innerHeight;
@@ -28,6 +29,92 @@
 		const cw = 2; //Default circle stroke width
 		const lw = 2; //Default line stroke width
 		const scale_divisor = 1000; //Divides coordinates used for drawing circles
+		
+		const part_array = [];
+		
+		const el_floor_up = document.getElementById("floor_up");
+		const el_floor_down = document.getElementById("floor_down");
+		
+		let scaling = false;
+		let active = false;
+	
+	//Classes
+		class XY
+		{
+			constructor(x, y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+		}
+		
+		let current;
+		
+		class Part
+		{
+			static current_part;
+			static current_index = 0;
+			
+			static path_to;
+			
+			constructor(id)
+			{
+				//Input variables
+					this.id = id;
+				
+				//SVG
+					//Get map
+						this.svg_string = get_map_svg(this.id);
+						this.svg_parser = new DOMParser();
+				       	this.svg_dom = this.svg_parser.parseFromString(this.svg_string, "image/svg+xml");
+						this.svg_elem = this.svg_dom.documentElement;
+					
+					//Draw groups
+						//Create group for drawing
+							this.draw_group = create_default_group(this.svg_elem, "Draw");
+					
+						//Create group for updatable drawing
+							this.removable_group = create_default_group(this.draw_group, "Things to remove when a new end point is selected");	
+							this.road_group = create_default_group(this.removable_group, "Roads");
+							this.circle_group = create_default_group(this.removable_group, "Circles");
+							
+					container.append(this.svg_elem);
+					this.hide_part();
+			}
+			
+			//Methods
+				set_current()
+				{
+					if (Part.current_part !== undefined)
+					{
+						Part.current_part.hide_part();
+					}
+					
+					Part.current_part = this;
+					
+					if (current !== undefined)
+					{
+						translate_set(current.x, current.y, Part.current_part.svg_elem);
+					}
+					
+					Part.current_part.show_part();
+				}
+				
+				set_path_to()
+				{
+					Part.path_to = this;
+				}
+				
+				hide_part()
+				{
+					hide(this.svg_elem);
+				}
+				
+				show_part()
+				{
+					show(this.svg_elem);
+				}
+		}
 		
 	//Objects
 		let zoom =
@@ -45,47 +132,21 @@
 			road: [["stroke-width", lw]],
 			group: [["class", "layer"]],
 		}
-	
-	//Classes
-		class XY
-		{
-			constructor(x, y)
-			{
-				this.x = x;
-				this.y = y;
-			}
-		}
-	
-	//SVG
-		//Get map
-			parse_pathname();
-
-			let svg_string = get_map_svg();
-			let svg_parser = new DOMParser();
-	       	let svg_dom = svg_parser.parseFromString(svg_string, "image/svg+xml");
-			let svg_elem = svg_dom.documentElement;
-			container.append(svg_elem);
 		
-		//Draw on map
-			//Create group for drawing
-				let draw_group = create_default_group(svg_elem, "Draw");
-				
-			//Create starting point
-				let start_coords = get_start_svg_coords();
-				let start_point = draw("circle", draw_group, undefined, templates.circle, [["cx", start_coords.x], ["cy", start_coords.y], ["fill", colors.circle_dark_green]]);
-				let start_viewport_coords = start_point.getBoundingClientRect();
-			
-			//Create group for updatable drawing
-				let removable_group = create_default_group(draw_group, "Things to remove when a new end point is selected");	
-				let road_group = create_default_group(removable_group, "Roads");
-				let circle_group = create_default_group(removable_group, "Circles");
-				
-	//Move
+	//Starting part
+		let json_point_data = JSON.parse(request(new URL(build_get_point_by_id_url(), base_api_url)));
+		part_array.push(new Part(json_point_data[0].building_part_id));
+		part_array[0].set_current();
+		part_array[0].set_path_to();
+		
+		let start_coords = get_start_svg_coords();
+		let start_point = draw("circle", Part.current_part.draw_group, undefined, templates.circle, [["cx", start_coords.x], ["cy", start_coords.y], ["fill", colors.circle_dark_green]]);
+		let start_viewport_coords = start_point.getBoundingClientRect();
+	
 		let initial = new XY();
-		let current = new XY(-start_viewport_coords.x - r + w / 2, -start_viewport_coords.y - r + h / 2);
-		let scaling = false;
-		let active = false;
-		translate_set(current.x, current.y, svg_elem);
+		current = new XY(-start_viewport_coords.x - r + w / 2, -start_viewport_coords.y - r + h / 2);
+		
+		translate_set(current.x, current.y, Part.current_part.svg_elem);
 
 	//EventListeners
 
@@ -115,7 +176,7 @@
 				initial.x = e.clientX - current.x * zoom.current;
 				initial.y = e.clientY - current.y * zoom.current;
 			}
-			if (e.target === container || e.target === svg_elem) //Elements that you can click to move map
+			if (e.target === container || e.target === Part.current_part.svg_elem) //Elements that you can click to move map
 			{
 				active = true;
 			}
@@ -135,7 +196,7 @@
 					current.x = (e.clientX - initial.x) / zoom.current;
 					current.y = (e.clientY - initial.y) / zoom.current;
 				}
-				translate_set(current.x, current.y, svg_elem);
+				translate_set(current.x, current.y, Part.current_part.svg_elem);
 			}
 		}
 		
@@ -179,7 +240,7 @@
 					current.x = (e.clientX - initial.x) / zoom.current;
 					current.y = (e.clientY - initial.y) / zoom.current;
 					
-					translate_set(current.x, current.y, svg_elem);
+					translate_set(current.x, current.y, Part.current_part.svg_elem);
 			}
 		}
 		
@@ -208,10 +269,9 @@
 			return xhr.response;
 		}
 		
-		function get_map_svg()
+		function get_map_svg(id)
 		{
-			let json_point_data = JSON.parse(request(new URL(build_get_point_by_id_url(), base_api_url)));
-			let json_building_part_data = JSON.parse(request(new URL(build_get_building_part_url(json_point_data[0].building_part_id), base_api_url)));
+			let json_building_part_data = JSON.parse(request(new URL(build_get_building_part_url(id), base_api_url)));
 			let json_map_svg_data = request(json_building_part_data[0].immutable_map_url);
 			return json_map_svg_data;
 		}
@@ -275,20 +335,51 @@
 		{
 			let previous_coord = start_coords;
 			
-			remove_elems(circle_group, "circle");
-			remove_elems(road_group, "line");
+			part_array.forEach((part) =>
+			{
+				remove_elems(part.circle_group, "circle");
+				remove_elems(part.road_group, "line");
+			})
 			
 			for (let i = 0; i < legend_len; i++)
 			{
 				let point_data = point_array[i].point;
+				
+				let draw_road = true;
+				if (point_data.building_part_id != Part.path_to.id)
+				{
+					if (part_exists(point_data.building_part_id))
+					{
+						part.set_path_to();
+					}
+					else
+					{
+						let part_to_push = new Part(point_data.building_part_id)						
+						part_array.push(part_to_push);
+						
+						if (part_array.length == 2)
+						{
+							el_floor_up.style.backgroundColor = "white";
+							el_floor_up.style.cursor = "pointer";
+						}
+						
+						part_to_push.set_path_to();
+					}
+					draw_road = false;
+				}
+
 				if (i > 0)
 				{
-					let removable_line = draw("line", road_group, undefined, templates.road, [["x1", previous_coord.x], ["y1", previous_coord.y], ["x2", point_data.x_value / scale_divisor], ["y2", point_data.y_value / scale_divisor], ["stroke", colors.road_gray]]);
-					let removable_point = draw("circle", circle_group, undefined, templates.circle, [["cx", point_data.x_value / scale_divisor], ["cy", point_data.y_value / scale_divisor], ["fill", colors.circle_gray]]);
+					if (draw_road)
+					{
+						let removable_line = draw("line", Part.path_to.road_group, undefined, templates.road, [["x1", previous_coord.x], ["y1", previous_coord.y], ["x2", point_data.x_value / scale_divisor], ["y2", point_data.y_value / scale_divisor], ["stroke", colors.road_blue]]);
+					}
+					
+					let removable_point = draw("circle", Part.path_to.circle_group, undefined, templates.circle, [["cx", point_data.x_value / scale_divisor], ["cy", point_data.y_value / scale_divisor], ["fill", colors.circle_blue]]);
 				}	
 				else
 				{
-					let removable_point = draw("circle", circle_group, undefined, templates.circle, [["cx", point_data.x_value / scale_divisor], ["cy", point_data.y_value / scale_divisor], ["fill", colors.circle_dark_green]]);
+					let removable_point = draw("circle", Part.path_to.circle_group, undefined, templates.circle, [["cx", point_data.x_value / scale_divisor], ["cy", point_data.y_value / scale_divisor], ["fill", colors.circle_dark_green]]);
 				}
 				
 				previous_coord = new XY(point_data.x_value / scale_divisor, point_data.y_value / scale_divisor);
@@ -315,4 +406,69 @@
 			let group = draw("g", append, undefined, templates.group, undefined);
 			let group_title = draw("title", group, name, undefined, undefined);
 			return group;
+		}
+		
+		function hide(el)
+		{
+			el.style.display = 'none';
+		}
+		
+		function show(el)
+		{
+			el.style.display = 'block';
+		}
+		
+	//Parts
+		function part_exists(id)
+		{
+			for (part of part_array)
+			{
+				if (part.id == id)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		function FloorUp()
+		{
+			if (part_array[Part.current_index + 1] !== undefined)
+			{
+				if (part_array[Part.current_index - 1] === undefined)
+				{
+					el_floor_down.style.backgroundColor = "white";
+					el_floor_down.style.cursor = "pointer";
+				}
+				
+				Part.current_index++;
+				part_array[Part.current_index].set_current();
+			}
+			
+			if (part_array[Part.current_index + 1] === undefined)
+			{
+				el_floor_up.style.backgroundColor = "gray";
+				el_floor_up.style.cursor = "default";
+			}
+		}
+		
+		function FloorDown()
+		{
+			if (part_array[Part.current_index - 1] !== undefined)
+			{
+				if (part_array[Part.current_index + 1] === undefined)
+				{
+					el_floor_up.style.backgroundColor = "white";
+					el_floor_up.style.cursor = "pointer";
+				}
+				
+				Part.current_index--;
+				part_array[Part.current_index].set_current();
+			}
+			
+			if (part_array[Part.current_index - 1] === undefined)
+			{
+				el_floor_down.style.backgroundColor = "gray";
+				el_floor_down.style.cursor = "default";
+			}
 		}
